81d479d56a2d3017c1718ab62ce28724f4e3f149
charu-dwivedi <charudwivedi455@gmail.com>
2015-08-29 19:13:53 -0400

    got columns working

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+var express = require('express');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/static'));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
