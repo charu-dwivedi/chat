3499d7e2272ae20efeafc848a38d932f20769696
Charu Dwivedi <charudwivedi455@gmail.com>
2016-10-09 21:21:50 -0400

    edited package.json for heroku

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var static = require('node-static');
 var http = require('http');
 var file = new(static.Server)();
+
 var app = http.createServer(function (req, res) {
-  file.serve(req, res);
+	file.serve(req, res);
 });
 
 var io = require('socket.io')(app);
